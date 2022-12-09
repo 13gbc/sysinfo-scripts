ee5cf2e2f871d5778cc1ef0b87cf7ae80b505e98
13gbc <118615125+13gbc@users.noreply.github.com>
2022-12-09 21:53:25 +0100

    Update info.sh

--- info.sh
+++ info.sh
@@ -6,24 +6,27 @@ echo "Hostname: $hostname"
 
 # Get the kernel version
 kernel=$(uname -r)
-echo "Kernel version: $kernel"
+echo -e "Kernel version: \t\t$kernel"
 
 # Get the system architecture
 architecture=$(uname -m)
-echo "System architecture: $architecture"
+echo -e "System architecture: \t\t$architecture"
 
 # Get the system uptime
 uptime=$(uptime -p)
-echo "System uptime: $uptime"
+echo -e "System uptime: \t\t\t$uptime"
 
 # Get the number of CPU cores
 cores=$(nproc)
-echo "Number of CPU cores: $cores"
+echo -e "Number of CPU cores: \t\t$cores"
 
 # Get the total amount of memory
 memory=$(free -h | grep Mem | awk '{print $2}')
-echo "Total amount of memory: $memory"
+echo -e "Total amount of memory: \t$memory"
 
 # Get the system's host IP address
 ip_address=$(hostname -I)
-echo "Host IP address: $ip_address"
+echo -e "Host IP address: \t\t$ip_address"
+
+file_system=$(df -h)
+echo -e "File System Disk Space Usage \n$file_system"
