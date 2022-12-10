4b59c5527c8f9256a677eacd15007c0588930dc2
13gbc <118615125+13gbc@users.noreply.github.com>
2022-12-10 13:23:09 +0100

    Create apple-info.sh

---
+++ apple-info.sh
@@ -0,0 +1,43 @@
+# Get the hostname of the computer
+hostname=$(hostname)
+
+# Get the operating system name and version
+os=$(sw_vers -productName)
+version=$(sw_vers -productVersion)
+
+# Get the total amount of physical memory in bytes
+memory=$(system_profiler SPHardwareDataType | grep '  Memory:' | awk '{print $2}')
+
+# Get the number of CPU cores
+cores=$(system_profiler SPHardwareDataType | grep '  Total Number of Cores:' | awk '{print $5}')
+
+# Get the system uptime
+uptime=$(uptime | awk -F'( |,|:)+' '{if ($7=="min") m=$6; else {if ($7~/^day/) {d=$6;h=$8;m=$9} else {h=$6;m=$7}}} {print d+0,"days,",h+0,"hours,",m+0,"minutes."}')
+
+# Get the list of disks and their properties
+disks=$(diskutil list)
+
+# Get the list of network adapters and their properties
+adapters=$(ifconfig -a)
+
+# Get the list of installed security updates
+updates=$(softwareupdate -l)
+
+# Print the system information
+echo "Hostname: $hostname"
+echo "Operating System: $os $version"
+echo "Total Memory: $memory GB"
+echo "CPU Cores: $cores"
+echo "Uptime: $uptime"
+
+# Print the disk information
+echo "Disks:"
+echo "$disks"
+
+# Print the network adapter information
+echo "Network Adapters:"
+echo "$adapters"
+
+# Print the security update information
+echo "Security Updates:"
+echo "$updates"
